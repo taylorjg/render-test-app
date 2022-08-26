166c5b0c5ea1d88c92f79298d2f52d72b3b5ff21
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-08-26 23:45:21 +0100

    Show toast on error, disable submit button when network call is active

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,14 @@ import { useState } from "react"
 import { Button, Chip, TextField } from "@mui/material"
 import { StyledButtonRow, StyledFormLayout } from "./Main.styles"
 import { useStringReverser } from "./use-string-reverser"
+import { useToast } from "./use-toast"
 
 export const Main = () => {
   const [inputText, setinputText] = useState("")
   const [reversedText, setReversedText] = useState()
+  const [callActive, setCallActive] = useState(false)
   const { reverseString } = useStringReverser()
+  const { renderToast, showError } = useToast()
 
   const onChangeInput = event => {
     const value = event.target.value
@@ -15,9 +18,16 @@ export const Main = () => {
 
   const onSubmit = async event => {
     event.preventDefault()
-    const outputText = await reverseString(inputText)
-    setReversedText(outputText)
-    setinputText("")
+    try {
+      setCallActive(true)
+      const outputText = await reverseString(inputText)
+      setReversedText(outputText)
+      setinputText("")
+    } catch (error) {
+      showError(error.message)
+    } finally {
+      setCallActive(false)
+    }
   }
 
   const onReset = () => {
@@ -37,13 +47,29 @@ export const Main = () => {
           onChange={onChangeInput}
         />
         <StyledButtonRow>
-          <Button variant="contained" size="small" type="submit">Reverse</Button>
-          <Button variant="contained" size="small" color="error" onClick={onReset}>Reset</Button>
+          <Button
+            variant="contained"
+            size="small"
+            type="submit"
+            disabled={callActive}
+          >
+            Reverse
+          </Button>
+          <Button
+            variant="contained"
+            size="small"
+            color="error"
+            onClick={onReset}
+          >
+            Reset
+          </Button>
         </StyledButtonRow>
+        {callActive && <div>CALL ACTIVE</div>}
         {reversedText && (
           <Chip label={reversedText} variant="outlined" />
         )}
       </StyledFormLayout>
+      {renderToast()}
     </form>
   )
 }
---
+++ src/use-toast.js
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import { Alert, Snackbar, Slide } from "@mui/material"
+
+export const useToast = () => {
+  const [open, setOpen] = useState(false)
+  const [message, setMessage] = useState("")
+  const [severity, setSeverity] = useState("success")
+
+  const showCommon = (message, severity) => {
+    setOpen(true)
+    setMessage(message)
+    setSeverity(severity)
+  }
+
+  const showSuccess = (message) => showCommon(message, "success")
+  const showInfo = (message) => showCommon(message, "info")
+  const showWarning = (message) => showCommon(message, "warning")
+  const showError = (message) => showCommon(message, "error")
+
+  const onClose = () => {
+    setOpen(false)
+    setMessage("")
+  }
+
+  const anchorOrigin = {
+    horizontal: "center",
+    vertical: "bottom"
+  }
+
+  const renderToast = () => {
+    return (
+      <Snackbar
+        open={open}
+        autoHideDuration={5000}
+        onClose={onClose}
+        anchorOrigin={anchorOrigin}
+        TransitionComponent={Slide}
+      >
+        <Alert onClose={onClose} severity={severity} variant="filled" sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    )
+  }
+
+  return {
+    renderToast,
+    showSuccess,
+    showInfo,
+    showWarning,
+    showError
+  }
+}

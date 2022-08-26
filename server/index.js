333a628f43293b3409a52444c4ba2178d3d21842
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-08-26 18:48:49 +0100

    Add server-side logging

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require("express")
-const path = require('path')
+const log = require("loglevel")
+const morgan = require("morgan")
+const path = require("path")
 
 const PORT = process.env.PORT ?? 3030
 const BUILD_FOLDER = path.resolve(__dirname, "..", "build")
 
 const main = () => {
+  log.setLevel("info")
   const app = express()
+  app.use(morgan("dev"))
   app.use(express.static(BUILD_FOLDER))
-  app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+  app.listen(PORT, () => log.info(`Listening on port ${PORT}`))
 }
 
 main()
